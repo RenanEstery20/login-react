8e440ca6d26fdd5eb37896dce912562194d2129f
Renan Estery <renanestery1@gmail.com>
2022-10-05 18:30:07 -0300

    ajuste

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,67 +1,74 @@
-import React,{ useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/auth";
-import {getCategories, getSpecifications} from "../../services/api"
+import { getCategories, getSpecifications } from "../../services/api"
 
 
-const HomePage = () =>{
-  const {logout, token} = useContext(AuthContext)
+const HomePage = () => {
+  const { logout } = useContext(AuthContext)
   const [categories, setCategories] = useState([])
   const [specifications, setSpecifications] = useState([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-   (async () => {
-    //const response = await getCategories()
-    const res = await getSpecifications(token)
+    (async () => {      
+      try {
+        const response = await getCategories()
+        //const res = await getSpecifications(localStorage.getItem("token"))
+       // console.log(token)
+      setCategories(response.data)
+      //setSpecifications(res.data)
+      setLoading(false)
+
+      } catch (error) {
+        console.log('Não autorizado');
+      }
+
       
-    
-    console.log(token)
-    //setCategories(response.data)
-    setSpecifications(res.data)
-    setLoading(false)
-   })()
+    })()
   }, [])
 
-  const handleLogout = () =>{
+  const handleLogout = () => {
     logout()
   }
 
-  if(loading){
+  if (loading) {
     return <div className="loading">Carregando...</div>
   }
 
-  return(
+  return (
     <>
-    <h1>Home Page</h1>
-    <button onClick={handleLogout}>Logout</button>
-    {
-      /*
-       <ul>
+      <h1>Home Page</h1>
+      <button onClick={handleLogout}>Logout</button>
       {
-        categories.map((category) => (
-          <li key={category.id}>
-            {category.name}
-          </li>
-        ))
+        
+         <ul>
+        {
+          categories.map((category) => (
+            <li key={category.id}>
+              {category.name}
+            </li>
+          ))
+        }
+      </ul>
+        
       }
-    </ul>
-      */
-    }
-   
-   {
-      <ul>
+
       {
-        specifications.map((specification) => (
-          <li key={specification.id}>
-            {specification.name}
-          </li>
-        ))
+        /*
+        <ul>
+          {
+            specifications.map((specification) => (
+              <li key={specification.id}>
+                {specification.name}
+              </li>
+            ))
+          }
+        </ul>
+        */
       }
-    </ul>
-   }
-   
+
     </>
-    
+
 
   )
 }
